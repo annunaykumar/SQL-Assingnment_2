4d1ce44de0a92a3164856c2b5a45665519d4bdd9
anunaykumar <annunay711998@gmail.com>
2022-09-01 10:27:00 +0530

    Add assingnment 2 SQL file

--- SQLQueryAssingnent2.1.b.sql
+++ SQLQueryAssingnent2.1.b.sql
@@ -1,3 +1,7 @@
-SELECT *
-FROM Project
-Natural Join Works_on;
+--SELECT *
+--FROM Project
+--Natural Join Works_on;
+
+select * 
+from Project  
+join Works_on  on Project.project_no=Works_on.project_no;
